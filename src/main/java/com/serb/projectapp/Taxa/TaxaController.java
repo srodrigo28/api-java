b8cba1526eb6ff1038f2ccae503b419f0c5b0a65
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-07-21 22:51:41 -0300

    updated-taxa

--- src/main/java/com/serb/projectapp/Taxa/TaxaController.java
+++ src/main/java/com/serb/projectapp/Taxa/TaxaController.java
@@ -1,6 +1,7 @@
 package com.serb.projectapp.Taxa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class TaxaController {
     
     @Autowired
